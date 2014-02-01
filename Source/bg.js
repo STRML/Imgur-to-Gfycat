7e77f151b3adeee0ed78af94bd2ec4e06d788197
Samuel Reed <sam@tixelated.com>
2014-02-01 11:00:05 +0800

    WIP converting embed to something smarter (chrome-only)

--- Source/bg.js
+++ Source/bg.js
@@ -1,6 +1,7 @@
 var gifRegex = /.*imgur.com\/.*\.gif(?:\?.*)?$/;
 
 blockGifLoading();
+findUnicorns();
 
 // Use webRequests API to block eligible gifs from loading to save bandwidth.
 function blockGifLoading(){
@@ -17,4 +18,24 @@ function blockGifLoading(){
 
 function shouldBlock(url){
   return gifRegex.test(url) && url.indexOf('?ignoreGfy') === -1;
+}
+
+
+// The occasional .jpg or .png is mislabeled and is actually a gif. Check content-type
+// headers and mark these things so we can gfy them.
+function findUnicorns(){
+  chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+      if (isUnicorn(details)) {
+        // this gif works in chrome only; see http://stackoverflow.com/a/15960901/832202
+        return { redirectUrl: 'data:image/gif;base64,R0lGODlhAQABAAAAACw=' };
+      }
+    },
+    {urls: ['*://*.imgur.com/*'], types: ['image']},
+    ['blocking']);
+}
+
+function isUnicorn(details) {
+  return !gifRegex.test(details.url) && details.responseHeaders && details.responseHeaders['content-type'] &&
+          details.responseHeaders['content-type'] === 'image/gif';
 }
